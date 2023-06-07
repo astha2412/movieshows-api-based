8376cbfd3d432ae500ca18b351fb5b621650a938
Astha <asthabarnwal@gmail.com>
2023-06-07 10:35:21 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ import MovieTicketForm from './components/useEffect/MovieTicketForm';
 function App() {
   return (
     <>
-    <Router>
+    <Router basename="/movieshows-api-based">
         <Routes>
-          <Route path="/" element={<MovieDetailsPage />} />
+          <Route exact path="/movieshows-api-based" element={<MovieDetailsPage />} />
           <Route path="/ticket-form" element={<MovieTicketForm />} />
         </Routes>
       </Router>
